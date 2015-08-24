092bb7b19bc99de80d2daebdd37fce730d365356
Gunnar Zötl <gz@tset.de>
2015-08-24 13:29:28 +0200

    clean s3ttings.php

--- a55ets/s3tt1nGs.php
+++ a55ets/s3tt1nGs.php
@@ -1,7 +1,7 @@
 <?php
 /*private constants*/
 define("DATA_FOLDER","__d4ta/");
-define("ADMIN","5dm2n");
+define("ADMIN","4dm1n");
 /*public constants*/
 define("BRAND",false);
 define("MULTI",false);
@@ -12,7 +12,7 @@ define("SR","/");
 /*DO NOT PUT this to "true" on a live site, it will reveal important information to your visitors*/
 define("DEBUG",false);
 /*this is a MD5 Hash of the actual password, use online tools like http://www.md5hashgenerator.com to find it out.*/
-define("PASS","55013e04093ca11507418fdd30b20241");
+define("PASS","21232f297a57a5a743894a0e4a801fc3");
 define("LANG","en");
 define("THEME_FOLDER","mad");
 define("IMAGE_FOLDER",'themes/'.THEME_FOLDER.'/images/pages'); // relative to SR
